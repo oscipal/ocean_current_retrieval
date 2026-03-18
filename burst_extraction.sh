1b900b94d30d805e907542e39e48b6b29b5f275b
oscipal <otto.scipal@gmail.com>
2026-03-18 18:07:47 +0100

    added sentinel-1 data

---
+++ burst_extraction.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+for i in {1..9}
+do
+  ScanSAR_burst_copy data/S1_data/desert/iw1_vv.slc data/S1_data/desert/iw1_vv.slc.par data/S1_data/desert/iw1_vv.slc.tops_par data/S1_data/desert/iw1_vv_burst$i.slc data/S1_data/desert/iw1_vv_burst$i.slc.par $i 1 - - > /dev/null 2>&1
+  echo "burst $i done"
+done
