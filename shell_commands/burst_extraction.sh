6421e34233b9edd557c5bf576672e5bd9c16da30
oscipal <otto.scipal@gmail.com>
2026-04-08 19:33:53 +0200

    updates

--- shell_commands/burst_extraction.sh
+++ shell_commands/burst_extraction.sh
@@ -2,6 +2,6 @@
 
 for i in {1..9}
 do
-  ScanSAR_burst_copy data/S1_data/desert/iw1_vv.slc data/S1_data/desert/iw1_vv.slc.par data/S1_data/desert/iw1_vv.slc.tops_par data/S1_data/desert/iw1_vv_burst$i.slc data/S1_data/desert/iw1_vv_burst$i.slc.par $i 1 - - > /dev/null 2>&1
+  ScanSAR_burst_copy data/S1_data/test2/iw1_vv.slc data/S1_data/test2/iw1_vv.slc.par data/S1_data/test2/iw1_vv.slc.tops_par data/S1_data/test2/iw1_vv_burst$i.slc data/S1_data/test2/iw1_vv_burst$i.slc.par $i 1 - - > /dev/null 2>&1
   echo "burst $i done"
 done
